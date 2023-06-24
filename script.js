3891cce7c6ee08b95fe904810061ae0b32aeb378
Vivek <vkmishra3012@gmail.com>
2023-06-24 10:54:30 +0530

    image-fix

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function greet() {
-    var x = document.getElementById('txt').value;
-    imgg.src = `https://source.unsplash.com/400x400/?${x}`
+function imageGenerate() {
+    let txt = document.getElementById("txt").value;
+    let url = `https://source.unsplash.com/400x400/?${txt}`;
+    let result = document.getElementById("result");
+    let welcomeMsg = document.getElementById("welcome-msg");
+
+    welcomeMsg.innerHTML = `Here is the result for ${txt}`;
+    result.innerHTML = `<img src="${url}" class="img-fluid">`;
 }
